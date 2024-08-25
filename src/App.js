513123132ee2cb0118c03fe198ccceb98cea49c6
Lorraine Thomas <thomas.lorraine1@gmail.com>
2024-08-25 19:21:03 -0400

    Included code from pure js project in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import WeatherApp from "./WeatherApp.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <WeatherApp />
     </div>
   );
 }
